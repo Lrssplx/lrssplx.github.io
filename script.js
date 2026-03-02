704405536000f6242cc6c3c7768e5c23c394337e
Larissa Oliveira <lrssplx@gmail.com>
2026-03-02 20:19:49 -0300

    v1.1

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll", () => {
-  reveals.forEach((el) => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-    const visible = 100;
-
-    if (elementTop < windowHeight - visible) {
-      el.classList.add("active");
-    }
-  });
-  const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-});
 });
